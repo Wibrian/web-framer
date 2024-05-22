b13802396b1d1c58b5821679ed279826b1688d61
Wibrian Alfathan <wibrianalfathan@gmail.com>
2024-05-22 23:53:19 +0700

    Update UI

--- src/app/components/HowItWorks.jsx
+++ src/app/components/HowItWorks.jsx
@@ -65,16 +65,6 @@ const howItWorksContent = {
       },
     },
     {
-      icon: BiBulb,
-      title: "Construction Administration",
-      description:
-        "Our journey begins with a spark of imagination and a commitment to turning dreams into tangible spaces. From concept to construction, we orchestrate a symphony of design",
-      btn: {
-        href: "#",
-        label: "Learn More",
-      },
-    },
-    {
       icon: BiOutline,
       title: "Post-Occupancy Evaluation",
       description:
@@ -99,35 +89,125 @@ const howItWorksContent = {
 
 export default function HowItWorks({ className }) {
   return (
-    <section className={`${className}`}>
-      <div className="container px-4 mx-auto">
-        <div className="lg:flex justify-center mb-20 lg:mb-36">
-          <div className="w-full lg:w-8/12 lg:flex gap-0 items-center">
-            <div className="lg:w-7/12 mb-5 lg:mb-0">
-              {howItWorksContent.heading.subtitle && (
-                <motion.span
-                  initial={{ opcatity: 0, y: 20 }}
-                  whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.5 } }}
-                  viewport={{ once: true }}
-                  className="uppercase tracking-[3px] text-sm mb-5 inline-block text-gray-500">
-                  {howItWorksContent.heading.subtitle}
-                </motion.span>
-              )}
-              {howItWorksContent.heading.title && (
-                <motion.h2
-                  initial={{ opcatity: 0, y: 20 }}
-                  whileInView={{ opacity: 1, y: 0, transition: { delay: 0.3, duration: 0.5 } }}
-                  viewport={{ once: true }}
-                  className="text-2xl lg:text-4xl ">
-                  {howItWorksContent.heading.title}
-                </motion.h2>
-              )}
+    <>
+      <section className={`${className}`}>
+        <div className="container px-4 mx-auto">
+          <div className="lg:flex justify-center mb-20 lg:mb-36">
+            <div className="w-full lg:w-8/12 lg:flex gap-0 items-center">
+              <div className="lg:w-7/12 mb-5 lg:mb-0">
+                {howItWorksContent.heading.subtitle && (
+                  <motion.span
+                    initial={{ opcatity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.5 } }}
+                    viewport={{ once: true }}
+                    className="uppercase tracking-[3px] text-sm mb-5 inline-block text-gray-500">
+                    {howItWorksContent.heading.subtitle}
+                  </motion.span>
+                )}
+                {howItWorksContent.heading.title && (
+                  <motion.h2
+                    initial={{ opcatity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0, transition: { delay: 0.3, duration: 0.5 } }}
+                    viewport={{ once: true }}
+                    className="text-2xl lg:text-4xl">
+                    {howItWorksContent.heading.title}
+                  </motion.h2>
+                )}
+              </div>
+              <div className="lg:w-5/12 self-end">
+                {howItWorksContent.heading.description && (
+                  <motion.p
+                    initial={{ opcatity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0, transition: { delay: 0.6, duration: 0.5 } }}
+                    viewport={{ once: true }}
+                    className="text-gray-500">
+                    {howItWorksContent.heading.description}
+                  </motion.p>
+                )}
+              </div>
             </div>
-            <div className="lg:w-5/12 self-end"></div>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 -mb-72 lg:w-10/12 mx-auto">
+            {howItWorksContent.steps.map((step, i) => {
+              i *= 0.2;
+              return (
+                <motion.div
+                  key={step.title}
+                  initial={{ opcatity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0, transition: { delay: i, duration: 0.5 } }}
+                  viewport={{ once: true }}
+                  whileHover={{ y: -10, transition: 0.1 }}
+                  className="group duration-300 pt-32 pl-10 pr-10 pb-20 bg-white relative overflow-hidden hover:bg-[#fb2576] hover:shadow-2xl">
+                  <span className="text-[200px] iniline-block z-[1] font-semibold absolute -top-[120px] opacity-5 left-0 leading-0">
+                    {step.number}
+                  </span>
+                  <div className="absolute top-10 right-10">
+                    <span className="text-3xl text-violet-600 duration-300 transition-all ease-in-out group-hover:text-white">
+                      <step.icon />
+                    </span>
+                  </div>
+                  <div className="relative z-40 flex gap-3 items-start">
+                    <div className="font-semibold duration-300 transition-all ease-in-out group-hover:text-white group-hover:text-opacity-50">
+                      {step.number}
+                    </div>
+                    <div>
+                      <h3 className="text-xl mb-4 duration-300 transition-all ease-in-out group-hover:text-white">{step.title}</h3>
+                      <p className="leading-relaxed text-[15px] text-gray-500 mb-7 duration-300 transition-all ease-in-out group-hover:text-white">
+                        {step.description}
+                      </p>
+                      <p>
+                        <Link
+                          href={step.btn.href}
+                          className="text-[12px] tracking-[2px] uppercase border-b-2 pb-2 inline-block border-violet-600 duration-300 trasition-all ease-in-out group-hover:border-white group-hover:text-white">
+                          {step.btn.label}
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                </motion.div>
+              );
+            })}
           </div>
         </div>
-        <div className=""></div>
-      </div>
-    </section>
+      </section>
+      <section className="pt-72 lg:pt-60 pb-32 bg-violet-600">
+        <div className="container px-4 mx-auto">
+          <div className="lg:flex justify-center">
+            <div className="w-full lg:w-8/12 flex gap-0 items-center">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                {howItWorksContent.features.map((feature, i) => {
+                  i *= 0.2;
+                  return (
+                    <motion.div
+                      key={feature.title}
+                      initial={{ opcatity: 0, y: 30 }}
+                      whileInView={{ opacity: 1, y: 0, transition: { delay: i, duration: 0.5 } }}
+                      viewport={{ once: true }}
+                      className="relative z-40 flex gap-3 items-start">
+                      <div>
+                        <span className="text-3xl text-white">
+                          <feature.icon />
+                        </span>
+                      </div>
+                      <div>
+                        <h3 className="text-lg mb-4 text-white">{feature.title}</h3>
+                        <p className="leading-relaxed text-[15px] text-white text-opacity-75 mb-7">{feature.description}</p>
+                        <p>
+                          <Link
+                            href={feature.btn.href}
+                            className="text-[12px] tracking-[2px] uppercase border-b-2 pb-2 inline-block text-white border-white border-opacity-25 duration-300 trasition-all ease-in-out hover:border-opacity-100">
+                            {feature.btn.label}
+                          </Link>
+                        </p>
+                      </div>
+                    </motion.div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <main>
       <Hero className={"pt-32 pb-0 lg:pb-10"} />
       <About className={"py-16 lg:py-32 overflow-hidden"} />
-      <HowItWorks />
+      <HowItWorks className={"py-16 !pt-32 lg:py-32"} />
     </main>
   );
 }
